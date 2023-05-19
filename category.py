081c8b16a6070a8c2dce1da39ee6a2f78207ba02
Neo <neohuiqixue@gmail.com>
2023-05-20 02:12:49 +0800

    implement identity, refactor code

--- category.py
+++ category.py
@@ -1,5 +1,11 @@
 from typing import Any
 
+"""
+Progress:
+- Implement basic category and morphisms, where a category is an object `catA = Cat(A)`
+- Change category code to superclass, now a category is any class `A(Category)` that extends `Category`
+"""
+
 class MorphismDomainError(Exception):
     pass
 
@@ -11,28 +17,22 @@ class MorphismUndefinedError(Exception):
 
 class Morphism:
 
-    def __init__(self, category, domain, codomain):
+    def __init__(self, category, domain, codomain, name=None):
         """
         - `domain` and `codomain` must be instances `category`
         - `category` msut be a subclass of `Category`
         """
         if type(domain) == category and type(codomain) == category:
-            self.category = category
             try:
-                category.morphisms.append(self)
+                category.morphisms
             except AttributeError:
-                category.morphisms = [self]
-            self.name = f'{category.__name__}_mor{len(category.morphisms)}'
+                category.morphisms = []
+            self.category = category
+            self.name = f'{category.__name__}_mor{len(category.morphisms)}' \
+                        if name is None else name
+            category.morphisms.append(self)
             self.domain = domain
             self.codomain = codomain
-            x, y = domain, codomain
-            def addSelf(d: dict, v):
-                try:
-                    d[v].add(self)
-                except KeyError:
-                    d[v] = {self}
-            addSelf(x.morphismsTo, y)
-            addSelf(y.morphismsFrom, x)
         else:
             raise MorphismCategoryError(
                 f"Morphism's domain & codomain must be instances of the category {category.name}")
@@ -43,24 +43,35 @@ class Morphism:
 
 class Category:
     def __init__(self):
-        self.morphismsTo = {}
-        self.morphismsFrom = {}
+        # self is an object in some category A, where A is a subclass of Category
+        self.identity = id = Morphism(
+            category=self.__class__, domain=self, codomain=self,
+            name=f'{self.__class__.__name__}_morId{self}'
+        )
+        self.morphismsTo = {self: {id}}
+        self.morphismsFrom = {self: {id}}
 
     @classmethod
     def anyObject(cls):
         newObject = cls()
-
-        category = cls
         try:
-            category.objects.append(newObject)
+            cls.objects.append(newObject)
         except AttributeError:
-            category.objects = [newObject]
+            cls.objects = [newObject]
         return newObject
 
     @classmethod    
     def anyMorphism(cls, domain, codomain) -> Morphism:
-        morphism = Morphism(category=cls, domain=domain, codomain=codomain)
-        return morphism
+        newMorphism = Morphism(category=cls, domain=domain, codomain=codomain)
+        x, y = domain, codomain
+        def addNewMorphism(d: dict, v):
+            try:
+                d[v].add(newMorphism)
+            except KeyError:
+                d[v] = {newMorphism}
+        addNewMorphism(x.morphismsTo, y)
+        addNewMorphism(y.morphismsFrom, x)
+        return newMorphism
 
 class SerialNamedType:
 
@@ -83,13 +94,13 @@ class SerialNamedType:
 
 class A(Category, SerialNamedType):
     def __init__(self):
-        Category.__init__(self)
         SerialNamedType.__init__(self)
+        Category.__init__(self)
 
 class B(Category, SerialNamedType):
     def __init__(self):
-        Category.__init__(self)
         SerialNamedType.__init__(self)
+        Category.__init__(self)
 
 def try_execute(f, x):
     try:
@@ -101,7 +112,7 @@ a0 = A.anyObject()
 a1 = A.anyObject()
 f = A.anyMorphism(a0, a1)
 g = A.anyMorphism(a0, a1)
-print(a0, a1, f, g, sep='\n')
+print(a0, a1, f, g, a0.identity, a1.identity, sep='\n')
 
 # catA = CategoryOf(A)
 # a1 = catA.anyObject()
--- try.py
+++ try.py
@@ -1,2 +1,19 @@
 x = y = 3
 print(x, y)
+
+class Parent:
+    @classmethod
+    def f(cls):
+        try:
+            cls.morphs
+        except:
+            cls.morphs = []
+    
+
+
+class Child(Parent):
+    pass
+
+# print(Child.morphs)
+print(Child.f())
+print(Child.morphs)
